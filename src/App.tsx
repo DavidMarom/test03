207e313aae47c1181fed3fd28d9347d054ff624c
David Marom <davidmarom@hotmail.com>
2025-03-13 21:11:02 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import http from './service/http.jsx'
 
 import './App.css'
 
-export function App() {
+export async function App() {
 
   const [products, setProducts] = useState<any[]>([])
-
-
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+  
   return (
     <view>
       <view className='App'>
         
-
+      <h1>Hi</h1>
       </view>
     </view>
   )
